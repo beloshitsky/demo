9bbacad77051e39e1d637e7a5a2db6838ef7e924
Stanislav Beloshitsky <beloshitsky.sa@gmail.com>
2022-11-03 21:49:19 +0600

    update

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -3,11 +3,13 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class Controller {
 
     @GetMapping()
     public String sayHello() {
-        return "Hello World!"+ " test 3";
+        return "Hello World!"+ " now on server: " + new Date();
     }
 }
